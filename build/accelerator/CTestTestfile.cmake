fea759396fdc2dcd0bb1ffaffe4d3ddd38fdfc68
Zachary Parks <1zp@ornl.gov>
2018-09-07 11:25:59 -0400

    repo cleanup (removing build)
    Signed-off-by: Zachary Parks <1zp@ornl.gov>

--- build/accelerator/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/skillapop/Documents/XACC-Development/xacc-qpp/accelerator
-# Build directory: /Users/skillapop/Documents/XACC-Development/xacc-qpp/build/accelerator
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/utils/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/skillapop/Documents/XACC-Development/xacc-qpp/utils
-# Build directory: /Users/skillapop/Documents/XACC-Development/xacc-qpp/build/utils
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
